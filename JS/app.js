5575d18fe0416f21ae8420dbb4d9f0fea7d7b630
Unknown <unknownmagma008@gmail.com>
2025-07-26 10:06:01 +0500

    26 July 2025 Saturday 10:05AM

--- JS/app.js
+++ JS/app.js
@@ -7,105 +7,23 @@ const day = document.querySelector(".day")
 const month = document.querySelector(".month")
 const ampm = document.querySelector(".ampm")
 
-const monthFinder = (monthNum) => {
-    let monthDay
-    switch (monthNum) {
-        case 0:
-            monthDay = "January"
-            break; 
-        case 1:
-            monthDay = "February"
-            break;
-        case 2:
-            monthDay = "March"
-            break;
-        case 3:
-            monthDay = "April"
-            break;
-        case 4:
-            monthDay = "May"
-            break;
-        case 5:
-            monthDay = "June"
-            break;
-        case 6:
-            monthDay = "July"
-            break;
-        case 7:
-            monthDay = "August"
-            break;
-        case 8:
-            monthDay = "September"
-            break;
-        case 9:
-            monthDay = "October"
-            break;
-        case 10:
-            monthDay = "November"
-            break;
-        case 11:
-            monthDay = "December"
-            break;
-        default:
-            alert("Error occured!!!")
-            break;
-    }
-    // console.log(monthDay)
-    return monthDay
-}
-
 const zeroAdder = (num) => {
     return (num < 10 ? `0${num}` : num)
 }
 
-const weekFinder = (number) => {
-    let weekday;
-    switch (number) {
-        case 1:
-            weekday = "Monday"
-            break;
-    
-        case 2:
-            weekday = "Tuesday"
-            break;
-    
-        case 3:
-            weekday = "Wednesday"
-            break;
-    
-        case 4:
-            weekday = "Thursday"
-            break;
-    
-        case 5:
-            weekday = "Friday"
-            break;
-    
-        case 6:
-            weekday = "Saturday"
-            break;
-    
-        case 7:
-            weekday = "Sunday"
-            break;
-    
-        default:
-            alert("Weekday error!!!")
-            break;
-    }
-    return weekday
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-}
+let weekdays = ["Nothing", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 setInterval(() => {
     let date = new Date
     let d = date.getDate()
     let y = date.getFullYear()
-    let w = weekFinder(date.getDay())
+    let w = weekdays[date.getDay()]
     let h = zeroAdder(date.getHours())
     let m = zeroAdder(date.getMinutes())
     let s = zeroAdder(date.getSeconds())
-    let mnth = monthFinder(date.getMonth())
+    let mnth = months[date.getMonth()]
     let apm = h > 12 ? "PM" : "AM"
     
     h = (h > 12 ? h - 12 : h)
@@ -119,4 +37,3 @@ setInterval(() => {
     day.innerText = d
     ampm.innerText = apm
 }, 1000);
-
